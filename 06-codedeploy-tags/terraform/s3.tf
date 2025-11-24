8cb66117c22c5dd67bd00e150b50eb7503573b6a
mito-201 <69384480+mito-201@users.noreply.github.com>
2025-11-24 11:33:18 +0900

    第5章:ファイルアップロードを追加

--- 06-codedeploy-tags/terraform/s3.tf
+++ 06-codedeploy-tags/terraform/s3.tf
@@ -13,3 +13,12 @@ resource "aws_s3_bucket_versioning" "artifact_versioning" {
     status = "Enabled" # バージョニングを有効化
   }
 }
+
+# コードパイプラインで配布するZipファイルを、S3バケットにアップロードする
+# コードパイプラインをterraformで作成すると、作成と同時に実行されるため、
+# アップロードは無効化している。有効化はお任せ。
+#resource "aws_s3_object" "object" {
+#  bucket = aws_s3_bucket.artifact.bucket
+#  key    = "install_git.zip"
+#  source = "../artifacts/install_git.zip"
+#}
